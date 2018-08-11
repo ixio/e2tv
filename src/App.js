cedd025b5166a069f25ab77687db08a97afb4101
Erwan Keribin <erwan.keribin@gmail.com>
2018-08-11 11:49:09 +0200

    Removes clutter and adds the name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Escape through the Void</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Your world is dead, you must travel through rifts in the Void in order to find a new world for your people.
         </p>
       </div>
     );
